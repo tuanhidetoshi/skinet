0d687d27aec3a2cb1ad5b905d3ee1b8aed155abf
tuanva <tuan.vu1802@gmail.com>
2021-08-27 15:34:50 +0700

    End of section 10

---
+++ client/src/app/shop/product-details/product-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.scss']
+})
+export class ProductDetailsComponent implements OnInit {
+  product!: IProduct;
+
+  constructor(private shopService : ShopService, private activateRoute: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadProduct()
+  }
+
+  loadProduct() {
+    const id = this.activateRoute.snapshot.paramMap.get('id')
+    if (id) {
+      this.shopService.getProduct(+id).subscribe(product => {
+        this.product = product
+      }, error => {
+        console.log(error)
+      })
+    }
+  }
+}
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -5,6 +5,7 @@ import { IPagination } from '../shared/models/pagination';
 import { IType } from '../shared/models/productTypes';
 import { map } from 'rxjs/operators';
 import { ShopParams } from '../shared/models/shopParams';
+import { IProduct } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
@@ -39,11 +40,15 @@ export class ShopService {
       );
   }
 
+  getProduct(id: number) {
+    return this.http.get<IProduct>(this.baseUrl + `products/${id}`)
+  }
+
   getBrands() {
-    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands');
+    return this.http.get<IBrand[]>(this.baseUrl + 'products/brands')
   }
 
   getTypes() {
-    return this.http.get<IType[]>(this.baseUrl + 'products/types');
+    return this.http.get<IType[]>(this.baseUrl + 'products/types')
   }
 }
